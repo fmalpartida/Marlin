880b6673f1a5e4fe0477dab210feba7930fdbca9
fmalpartida <fmalpartida@gmail.com>
2015-03-14 21:46:49 +0100

    Setting up end-stops for inductive sensor.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -420,7 +420,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER -35
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.15
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.45
 
   #define Z_RAISE_BEFORE_HOMING 2       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case

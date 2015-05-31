1962d26db36480c4103deccc4c4872cc5afdc1c5
fmalpartida <fmalpartida@gmail.com>
2015-05-31 23:19:46 +0200

    Updated buffer size.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -404,7 +404,7 @@ const unsigned int dropsegments=5; //everything with less than this number of st
 
 //The ASCII buffer for receiving from the serial:
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 5
+#define BUFSIZE 4
 
 // Bad Serial-connections can miss a received command by sending an 'ok'
 // Therefore some clients abort after 30 seconds in a timeout.
